1e0512041e28e1b85af89fd046293360c35e3c44
Thallys Reis <thallys770@gmail.com>
2023-08-21 20:56:06 -0300

    POST and PUT methods implemented

--- backend/src/main/java/com/project/crud/resources/ClientResource.java
+++ backend/src/main/java/com/project/crud/resources/ClientResource.java
@@ -1,14 +1,19 @@
 package com.project.crud.resources;
 
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.project.crud.dto.ClientDTO;
 import com.project.crud.services.ClientService;
@@ -34,27 +39,19 @@ public class ClientResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	//@GetMapping
-	//public ResponseEntity<Page<ClientDTO>> findAll(){}
-	
-	
-	
-	
-	/*
-	@GetMapping(value = "/{id}" )
-	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
-		ClientDTO dto = service.findById(id);
-		return ResponseEntity.ok().body(dto);
-		
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
-	*/
-	
-	//@PostMapping
-	//public ResponseEntity<ClientDTO> insert(){}
-	
 
-	//@PutMapping
-	//public ResponseEntity<ClientDTO> update(){}
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
+		dto = service.update(id,dto);
+		return ResponseEntity.ok().body(dto);
+	}
 
 	
 	//@DeleteMapping
